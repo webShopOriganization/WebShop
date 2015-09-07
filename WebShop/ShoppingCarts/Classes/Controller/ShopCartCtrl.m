c5f601dc76300ac3cf9366a4306170cd50a0c3f4
lipan <pan.li@chutong.cn>
2015-09-07 18:15:27 +0800

    add cell、view

---
+++ WebShop/ShoppingCarts/Classes/Controller/ShopCartCtrl.m
@@ -0,0 +1,13 @@
+//
+//  ShopCartCtrl.m
+//  WebShop
+//
+//  Created by chutong-mini-201401 on 15/9/7.
+//  Copyright (c) 2015年 chutong. All rights reserved.
+//
+
+#import "ShopCartCtrl.h"
+
+@implementation ShopCartCtrl
+
+@end
---
+++ WebShop/ShoppingCarts/Classes/View/Cell/LoadHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  LoadHeaderView.m
+//  WebShop
+//
+//  Created by chutong-mini-201401 on 15/9/7.
+//  Copyright (c) 2015年 chutong. All rights reserved.
+//
+
+#import "LoadHeaderView.h"
+
+@implementation LoadHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
